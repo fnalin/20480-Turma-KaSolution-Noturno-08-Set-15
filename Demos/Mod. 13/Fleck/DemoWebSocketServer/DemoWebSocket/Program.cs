9ba5ca6e11750691c6021ca6b3bfe17cf6009cf7
Fabiano Nalin <fnalin@msn.com>
2015-09-16 22:05:36 -0300

    Add demos Mod. 13 só com o Fleck
    Amanhã faremos com signalR

---
+++ Demos/Mod. 13/Fleck/DemoWebSocketServer/DemoWebSocket/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fleck;
+
+namespace DemoWebSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webSocketServer = new WebSocketServer("ws://127.0.0.1:8081");
+            var sockets = new List<IWebSocketConnection>();
+
+            webSocketServer.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    sockets.Add(socket);
+                };
+
+                socket.OnMessage = mensagem =>
+                {
+                    foreach (var s in sockets)
+                    {
+                        s.Send(mensagem);
+                    }
+                };
+
+                socket.OnClose = () =>
+                {
+                    sockets.Remove(socket);
+                };
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
